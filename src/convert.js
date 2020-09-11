fd57f8e02ece40e0c0caf1d5bc5bf62da1a3b6a7
Grant Eadie <grantleadie@gmail.com>
2020-09-11 15:39:46 -0700

    convert any currency to any other currency

--- src/convert.js
+++ src/convert.js
@@ -1,10 +1,15 @@
 export default class Convert{
-  constructor(amount, currency) {
+  constructor(amount, currency, inputCurrency) {
     this.amount = amount;
     this.currency = currency;
+    this.inputCurrency = inputCurrency
   }
 
   fromDollars() {
     return (this.amount * this.currency).toFixed(2);
+  }
+
+  fromAnything() {
+    return (this.amount * (this.currency / this.inputCurrency).toFixed(2));
   }
 }
--- src/main.js
+++ src/main.js
@@ -7,21 +7,21 @@ import Convert from './convert.js'
 
 function clearFields() {
   $('#amount').val("");
-  $('#currency1').val("");
 }
 
 $(document).ready(function() {
   $('#getSubmit').click(function() {
     let amount = $('#amount').val();
-    let inputtedCurrency = $('#currency1').val();
+    let outputCurrency = $('#currency1').val();
+    let inputCurrency = $('#currencyInput').val();
     clearFields();
 
     let promise = Currency.getCurrency();
     promise.then(function(response) {
       const body = JSON.parse(response);
       
-      let conversion = new Convert(amount, body.conversion_rates[inputtedCurrency]);
-      $('.showOutput').text((conversion.fromDollars()) + " " + inputtedCurrency);
+      let conversion = new Convert(amount, body.conversion_rates[outputCurrency], body.conversion_rates[inputCurrency]);
+      $('.showOutput').text((conversion.fromAnything()) + " " + outputCurrency);
 
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
